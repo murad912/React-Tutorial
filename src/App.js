5558e9e268d225448f420ee113a54ea432bb2e00
Murad Abubeker <muradabdulmalik@gmail.com>
2022-07-10 17:14:24 -0500

    react exercise

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Greeting from './Greeting';
+
+import {HelloComponentClass} from './pages/HelloComponentClass';
+import {HelloComponentFunction} from './pages/HelloComponentFunction';
+import {HelloComponentSimpleFunction} from './pages/HelloComponentSimpleFunction';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+         <HelloComponentClass />
+        <HelloComponentFunction />
+        <HelloComponentSimpleFunction />
       </header>
     </div>
   );
---
+++ src/Greeting.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Greeting() {
+    let isMorning = (new Date()).getHours() < 12;
+    let greetingElement = isMorning
+        ? <h3>Good Morning</h3>
+        : <h3>Good Evening</h3>;
+    return greetingElement;
+}
+
+export default Greeting;
